19af5fcc49d33fb78b3c46a363604fbd4b9a4799
shristy <shristychaudhary4679@gmail.com>
2025-02-24 23:10:07 +0530

    fixed typo in regex

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,15 @@ module.exports = {
   module: {
     rules: [
         {
-            test: "/\.css$/i",
+            test: /\.css$/i,
             use: ["style-loader", "css-loader"],
         },
         {
-            test: "/\.html$/i",
+            test: /\.html$/i,
             loader: "html-loader",
         },
         {
-            test: "/\.(png|svg|jpg|jpeg|gif)$/i",
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: "asset/resource",
         }
     ],
