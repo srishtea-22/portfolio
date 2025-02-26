48e4574818534f24b5f36ab10f066f0254da4441
shristy <shristychaudhary4679@gmail.com>
2025-02-26 19:57:33 +0530

    added animation trigger for skills

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css"
 import { animateOnLoad } from "./modules/ui"
+import { observer } from "./modules/ui";
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
 animateOnLoad(".letter");
